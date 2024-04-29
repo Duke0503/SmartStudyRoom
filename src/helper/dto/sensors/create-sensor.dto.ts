deaeb74734b040b7e68e7bc1eb2ecfd575f0bbee
Duke0503 <minhducgl0503@gmail.com>
2024-04-29 21:51:48 +0700

    Update API Sensors

---
+++ src/helper/dto/sensors/create-sensor.dto.ts
@@ -0,0 +1,15 @@
+export class CreateSensorDto {
+  name: string;
+
+  type: string;
+
+  sound_data: string;
+
+  temp_data: string;
+
+  light_data: string;
+
+  camera_data: string;
+
+  user_ID: number;
+}
